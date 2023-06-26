5d2dd5e266d35ce18259b45fabc5d1d5e219ab5e
Jhonny Lima <lima.jhonny95@gmail.com>
2023-06-25 23:45:21 -0300

    refactor: finish layout of NewMeal screen

--- src/screens/NewMeal/index.tsx
+++ src/screens/NewMeal/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import {
   Container,
   Form,
@@ -7,13 +9,27 @@ import {
 } from './styles';
 
 import { Body } from '@components/Body';
-import { ButtonIcon } from '@components/ButtonIcon';
 import { DescriptionInput } from '@components/DescriptionInput';
 import { Header } from '@components/Header';
+import { IconButton } from '@components/IconButton';
 import { Input } from '@components/Input';
 import { Label } from '@components/Label';
+import { MealTypeButton } from '@components/MealTypeButton';
 
 export function NewMeal() {
+  const [isInDiet, setIsInDiet] = useState(false);
+  const [isOutOfDiet, setIsOutOfDiet] = useState(false);
+
+  function handleInDietType() {
+    setIsInDiet(prevState => !prevState);
+    setIsOutOfDiet(false);
+  }
+
+  function handleOutOfDietType() {
+    setIsOutOfDiet(prevState => !prevState);
+    setIsInDiet(false);
+  }
+
   return (
     <Container>
       <Header
@@ -46,12 +62,24 @@ export function NewMeal() {
             <Label label='Está dentro da dieta?' />
 
             <Row>
+              <MealTypeButton
+                onPress={handleInDietType}
+                title='Sim'
+                type={isInDiet ? 'IN_DIET' : undefined}
+                iconType='IN_DIET'
+              />
 
+              <MealTypeButton
+                onPress={handleOutOfDietType}
+                title='Não'
+                type={isOutOfDiet ? 'OUT_OF_DIET' : undefined}
+                iconType='OUT_OF_DIET'
+              />
             </Row>
           </FormItemContainer>
 
           <ButtonView>
-            <ButtonIcon
+            <IconButton
               description='Cadastrar refeição'
             />
           </ButtonView>
