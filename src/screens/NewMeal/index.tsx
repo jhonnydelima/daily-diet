e2ec8f31ece01bec4c6009d6645aba53f155f46a
Jhonny Lima <lima.jhonny95@gmail.com>
2023-06-27 23:14:24 -0300

    feat: add date time picker to NewMeal screen

--- src/screens/NewMeal/index.tsx
+++ src/screens/NewMeal/index.tsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { format } from 'date-fns';
+import { ptBR } from "date-fns/locale"
 
 import {
   Container,
@@ -17,8 +20,40 @@ import { Label } from '@components/Label';
 import { MealTypeButton } from '@components/MealTypeButton';
 
 export function NewMeal() {
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [isDatePickerVisible, setIsDatePickerVisible] = useState(false);
+  const [isTimePickerVisible, setIsTimePickerVisible] = useState(false);
   const [isInDiet, setIsInDiet] = useState(false);
   const [isOutOfDiet, setIsOutOfDiet] = useState(false);
+
+  function showDatePicker() {
+    setIsDatePickerVisible(true);
+  };
+
+  function hideDatePicker() {
+    setIsDatePickerVisible(false);
+  };
+
+  function handleConfirmDate(date: Date) {
+    const formattedDate = format(date, "dd/MM/yyyy", { locale: ptBR });
+    setDate(formattedDate);
+    hideDatePicker();
+  };
+
+  function showTimePicker() {
+    setIsTimePickerVisible(true);
+  };
+
+  function hideTimePicker() {
+    setIsTimePickerVisible(false);
+  };
+
+  function handleConfirmTime(time: Date) {
+    const formattedTime = format(time, "HH:mm");
+    setTime(formattedTime);
+    hideTimePicker();
+  };
 
   function handleInDietType() {
     setIsInDiet(prevState => !prevState);
@@ -49,12 +84,32 @@ export function NewMeal() {
           <Row>
             <FormItemContainer>
               <Label label='Data' />
-              <Input />
+              <Input
+                onPressIn={showDatePicker}
+                value={date.toString()}
+              />
+
+              <DateTimePickerModal
+                isVisible={isDatePickerVisible}
+                mode="date"
+                onConfirm={handleConfirmDate}
+                onCancel={hideDatePicker}
+              />
             </FormItemContainer>
 
             <FormItemContainer>
               <Label label='Hora' />
-              <Input />
+              <Input
+                onPressIn={showTimePicker}
+                value={time.toString()}
+              />
+
+              <DateTimePickerModal
+                isVisible={isTimePickerVisible}
+                mode="time"
+                onConfirm={handleConfirmTime}
+                onCancel={hideTimePicker}
+              />
             </FormItemContainer>
           </Row>
 
