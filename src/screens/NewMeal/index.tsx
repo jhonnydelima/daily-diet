d3f55e3820cbff4542632faafb255273237137c6
Jhonny Lima <lima.jhonny95@gmail.com>
2023-07-03 01:37:33 -0300

    refactor: add form validation and save meal to storage

--- src/screens/NewMeal/index.tsx
+++ src/screens/NewMeal/index.tsx
@@ -1,16 +1,15 @@
+import 'react-native-get-random-values';
 import { useState } from 'react';
+import { Alert } from 'react-native';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import { useNavigation } from '@react-navigation/native';
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
+import { v4 as uuidv4 } from 'uuid';
+import { object, string, mixed } from 'yup';
 
-import {
-  Container,
-  Form,
-  Row,
-  FormItemContainer,
-  ButtonView
-} from './styles';
+import { mealCreate } from '@storage/meal/mealCreate';
+import { FeedbackTypeStyleProps } from '@screens/Feedback/styles';
 
 import { Body } from '@components/Body';
 import { DescriptionInput } from '@components/DescriptionInput';
@@ -19,9 +18,21 @@ import { IconButton } from '@components/IconButton';
 import { Input } from '@components/Input';
 import { Label } from '@components/Label';
 import { MealTypeButton } from '@components/MealTypeButton';
-import { FeedbackTypeStyleProps } from '@screens/Feedback/styles';
+
+import { Container, Form, Row, FormItemContainer, ButtonView } from './styles';
+
+const mealSchema = object({
+  uuid: string().required(),
+  name: string().required('O campo nome é obrigatório.'),
+  description: string().required('O campo descrição é obrigatório.'),
+  date: string().required('Selecione a data.'),
+  time: string().required('Selecione a hora.'),
+  type: mixed<'IN_DIET' | 'OUT_OF_DIET'>().required('Selecione um tipo de refeição.'),
+});
 
 export function NewMeal() {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
   const [date, setDate] = useState('');
   const [time, setTime] = useState('');
   const [isDatePickerVisible, setIsDatePickerVisible] = useState(false);
@@ -29,10 +40,6 @@ export function NewMeal() {
   const [mealType, setMealType] = useState<FeedbackTypeStyleProps>(undefined);
 
   const navigation = useNavigation();
-
-  function handleGiveFeedback() {
-    navigation.navigate('feedback', { type: mealType });
-  }
 
   function showDatePicker() {
     setIsDatePickerVisible(true);
@@ -66,6 +73,30 @@ export function NewMeal() {
     setMealType(type);
   }
 
+  async function handleAddMeal() {
+    const newMeal = {
+      uuid: uuidv4(),
+      name,
+      description,
+      date,
+      time,
+      type: mealType,
+    }
+
+    try {
+      const parsedMeal = await mealSchema.validate(
+        newMeal,
+        { strict: true },
+      );
+
+      await mealCreate(parsedMeal);
+      navigation.navigate('feedback', { type: mealType });
+    } catch (error) {
+      Alert.alert("Refeição", error.message);
+      console.log(error);
+    }
+  }
+
   return (
     <Container>
       <Header
@@ -77,10 +108,16 @@ export function NewMeal() {
       <Body>
         <Form>
           <Label label='Nome' />
-          <Input />
+          <Input
+            value={name}
+            onChangeText={setName}
+          />
 
           <Label label='Descrição' />
-          <DescriptionInput />
+          <DescriptionInput
+            value={description}
+            onChangeText={setDescription}
+          />
 
           <Row>
             <FormItemContainer>
@@ -136,7 +173,7 @@ export function NewMeal() {
 
           <ButtonView>
             <IconButton
-              onPress={handleGiveFeedback}
+              onPress={handleAddMeal}
               description='Cadastrar refeição'
             />
           </ButtonView>
