a76822cab31b74d8b0986fa5454cafad1a0263ef
Jhonny Lima <lima.jhonny95@gmail.com>
2023-07-03 01:36:13 -0300

    feat: add helper method to generate section list

---
+++ src/utils/SectionListGenerator.ts
@@ -0,0 +1,34 @@
+import { MealStorageDTO } from '@storage/meal/MealStorageDTO';
+
+export function generate(meals: MealStorageDTO[]) {
+	const dates = getDates(meals);
+	const sortedList = sortMealsByDate(dates, meals);
+	return sortedList;
+}
+
+function getDates(meals: MealStorageDTO[]) {
+	const dates: string[] = meals.map(({ date }) => date).sort();
+  return dates;
+}
+
+function sortMealsByDate(dates: string[], meals: MealStorageDTO[]) {
+	const sorted = dates.map(date => {
+    return {
+      date: date,
+      data: meals
+        .filter(meal => meal.date === date)
+        .sort((a, b) => {
+          if (a.time < b.time) {
+            return -1;
+          }
+          if (a.time > a.time) {
+            return 1;
+          }
+        
+          return 0;
+        })
+    }
+  });
+  
+  return sorted;
+}
