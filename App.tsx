ca33bc63affbd8fd2e369199e50bd403715d50de
Jhonny Lima <lima.jhonny95@gmail.com>
2023-07-09 03:19:37 -0300

    chore: add libs related to svg

--- App.tsx
+++ App.tsx
@@ -16,7 +16,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar
-        style='auto'
+        style='dark'
         backgroundColor='transparent'
         translucent
       />
